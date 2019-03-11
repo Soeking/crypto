8ab2a57d9fc6ab961a92120e1e875d0384e320c0
Soeking <great.from.g@gmail.com>
2019-03-12 00:02:44 +0900

    vigenere

--- src/vigenere.jl
+++ src/vigenere.jl
@@ -1,8 +1,4 @@
-function vigenere()
-    print("C:")
-    cipher=readline()
-    print("key:")
-    key=readline()
+function vigenere(cipher::String,key::String)
     loop=length(key)
     plain=""
 
@@ -11,21 +7,21 @@ function vigenere()
         if idx==0
             idx+=loop
         end
-        x=cipher[i]-Int(key[idx])
+        x=cipher[i]
         if isuppercase(cipher[i])
-            if Int(x)<0
+            if x<uppercase(key[idx])
                 x+=26
             end
             x+=65
+            x-=Int(uppercase(key[idx]))
         else
-            if Int(x)<0
+            if x<key[idx]
                 x+=26
             end
             x+=97
+            x-=Int(key[idx])
         end
         plain*=x
     end
     println(plain)
 end
-
-vigenere()
